d51cdc32b88158083721b8c85b90b96935386cac
Lennert Jansen <lennertjansen95@gmail.com>
2024-07-28 21:49:37 +0200

    added line reading logic to interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -4,3 +4,12 @@ import sys
 
 # read arguments
 program_filepath = sys.argv[1]
+
+###########################
+#     Tokenize Program
+###########################
+
+# read file lines
+program_lines = []
+with open(program_filepath, "r") as program_file:
+    program_lines = [line.strip() for line in program_file.readlines()]
